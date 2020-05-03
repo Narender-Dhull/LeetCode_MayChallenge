85ab5c001bf86b02a4138ce0f0e8e538a3101f89
Narender Dhull <narender.dhull@hotmail.co.uk>
2020-05-03 14:21:06 +0530

    https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3316/

---
+++ LeetCode_May/FirstBadVersion/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace FirstBadVersion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution _solution = new Solution();
+            int answer = _solution.FirstBadVersion(20);
+            Console.WriteLine(answer.ToString());
+            Console.WriteLine("Hello World!");
+        }
+        public class VersionControl
+        {
+            public bool IsBadVersion(int version)
+            {
+                return version == 9;
+            }
+        }
+        public class Solution : VersionControl
+        {
+            public int FirstBadVersion(int n)
+            {
+                int left = 1;
+                int right = n;
+                while (left < right)
+                {
+                    int mid = left + (right - left) / 2;
+                    if (IsBadVersion(mid))
+                    {
+                        right = mid;
+                    }
+                    else
+                    {
+                        left = mid + 1;
+                    }
+                }
+                return left;
+            }
+        }
+    }
+}
