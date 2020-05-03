f0c2dee6069267c153319df23672d8dc75a2a72b
Narender Dhull <narender.dhull@hotmail.co.uk>
2020-05-03 14:30:25 +0530

    https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3318/

---
+++ LeetCode_May/RansomNote/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RansomNote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CanConstruct("aa","aab"));
+            Console.WriteLine("Hello World!");
+        }
+        public static bool CanConstruct(string ransomNote, string magazine)
+        {
+            List<char> ransomeLettersList = new List<char>(ransomNote);
+
+            foreach (char c in magazine)
+            {
+                if (ransomeLettersList.Count == 0)
+                {
+                    return true;
+                }
+
+                int index = ransomeLettersList.IndexOf(c);
+                if (index >= 0) { ransomeLettersList.RemoveAt(index); };
+            }
+
+            if (ransomeLettersList.Count > 0)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
