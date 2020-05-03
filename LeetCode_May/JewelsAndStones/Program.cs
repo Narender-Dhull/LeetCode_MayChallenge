df9a0355a1b99f5784e5e8e66a792446f4ad568f
Narender Dhull <narender.dhull@hotmail.co.uk>
2020-05-03 14:28:24 +0530

    https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3317/

---
+++ LeetCode_May/JewelsAndStones/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace JewelsAndStones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NumJewelsInStones("aA", "aAAbbbb"));
+            Console.WriteLine("Hello World!");
+        }
+        public static int NumJewelsInStones(string J, string S)
+        {
+            int sum = 0;
+            char[] Jewels = J.ToCharArray();
+            char[] JewelsIHave = S.ToCharArray();
+
+            foreach (char c in Jewels)
+            {
+                foreach (char j in JewelsIHave)
+                {
+                    if (j.Equals(c))
+                    {
+                        sum++;
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+}
