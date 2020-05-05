99f9f4f4ef839027b520145c688807edd85d7ed8
Narender Dhull <narender.dhull@hotmail.co.uk>
2020-05-05 19:55:35 +0530

    https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3320/

---
+++ LeetCode_May/FirstUniqueCharacterInAString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace FirstUniqueCharacterInAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FirstUniqChar("eebbbooolaaaaa"));
+            Console.ReadKey();
+        }
+        public static int FirstUniqChar(string s)
+        {
+            int[] count = new int[26];
+            foreach (char c in s)
+                count[c - 'a']++;
+            for (int i = 0; i < s.Length; i++)
+                if (count[s[i] - 'a'] == 1)
+                    return i;
+            return -1;
+        }
+    }
+
+}
