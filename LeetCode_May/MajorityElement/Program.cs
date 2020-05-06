9edb69c344aa50820c6f7beed8c2f046af33b08a
Narender Dhull <narender.dhull@hotmail.co.uk>
2020-05-06 13:48:48 +0530

    https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3321/

---
+++ LeetCode_May/MajorityElement/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MajorityElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = { 2, 2, 1, 1, 1, 2, 2 };
+            Console.WriteLine(MajorityElement(input));
+            Console.ReadKey();
+        }
+        public static int MajorityElement(int[] nums)
+        {
+            Dictionary<int, int> keyValuePairs = new Dictionary<int, int>();
+            int compareLen = nums.Length / 2;
+            foreach (var item in nums)
+            {
+                if (keyValuePairs.ContainsKey(item))
+                {
+                    keyValuePairs[item]++;
+                }
+                else
+                {
+                    keyValuePairs.Add(item, 1);
+                }
+            }
+
+            foreach (var item in keyValuePairs)
+            {
+                if (item.Value > compareLen)
+                {
+                    return item.Key;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
