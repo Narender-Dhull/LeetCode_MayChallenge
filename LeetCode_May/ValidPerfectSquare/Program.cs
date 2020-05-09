7a24d63b6267152d84f330b3c2c78936fab88868
Narender Dhull <narender.dhull@hotmail.co.uk>
2020-05-09 14:26:37 +0530

    https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/535/week-2-may-8th-may-14th/3324/

---
+++ LeetCode_May/ValidPerfectSquare/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ValidPerfectSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPerfectSquare(14));
+            Console.ReadKey();
+        }
+        public static bool IsPerfectSquare(int num)
+        {
+            return (Math.Sqrt(num) % 1) < 0.001;
+        }
+    }
+}
